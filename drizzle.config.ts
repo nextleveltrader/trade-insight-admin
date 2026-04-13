a20f725c8eab53d5144a79d04b83d5f4de8f9ddb
nextleveltrader <nextleveltrader360@gmail.com>
2026-04-14 00:57:02 +0600

    Updated env to process.env for Vercel compatibility

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,12 @@
+import 'dotenv/config'; // এটি আপনার .env ফাইলটি রিড করবে
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   schema: './src/db/schema.ts',
   out: './migrations',
-  dialect: 'sqlite',
+  dialect: 'turso', // 'sqlite' এর বদলে 'turso' হবে
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+    authToken: process.env.DATABASE_AUTH_TOKEN!,
+  },
 });
